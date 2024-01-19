14c9d327686b4af3eb1a0d16b790a9d900d29282
pascalecodes <pdelauna@gmail.com>
2024-01-18 20:08:06 -0800

    testing auth, broken

--- server.js
+++ server.js
@@ -5,30 +5,49 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("express-flash");
+
+const methodOverride = require("method-override");
 const MemwaQuestion = require('./models/questions')
 const { ensureAuth, ensureGuest } = require("./middleware/auth");
 //require('dotenv').config()
+
 require("dotenv").config({ path: "./config/.env" });
 
+// Passport config
+require("./config/passport")(passport);
 
 //Set the middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(express.json());
 
-// Passport config
-require("./config/passport")(passport);
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
+app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  );
+  
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 //Connect to the database
 mongoose.connect(process.env.DB_STRING, 
     {useNewUrlParser: true}, 
     () => (console.log(`Connected to database: ${mongoose.connection.name}`))
 )
-
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 //get method
